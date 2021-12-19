36630cd4fd5014f3b7ce9b3a8a6c6ce58b55a824
darken <darken@darken.eu>
2021-12-19 18:30:29 +0100

    v1

---
+++ arduino-weather-indoor/src/main.cpp
@@ -0,0 +1,265 @@
+#include "Adafruit_Si7021.h"
+#include "uMQTTBroker.h"
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include <OneButton.h>
+#include <SPI.h>
+#include <Wire.h>
+
+/*
+ * Your WiFi config here
+ */
+char ssid[] = ""; // your network SSID (name)
+char pass[] = "";     // your network password
+bool WiFiAP = false;                // Do yo want the ESP as AP?
+
+long outdoorLastSeenMillis = 0;
+long outdoorUptimeMillis = 0;
+int outdoorWifiRssi = 0;
+float outdoorBatteryVoltage = 0;
+float outdoorBatteryPercent = 0;
+
+float outdoorSensorTempCelsus = 0;
+float outdoorSensorHumidityPercent = 0;
+float outdoorSensorPressurehPa = 0;
+
+/*
+ * Custom broker class with overwritten callback functions
+ */
+class myMQTTBroker : public uMQTTBroker {
+public:
+    virtual bool onConnect(IPAddress addr, uint16_t client_count) {
+        Serial.println(addr.toString() + " connected");
+        return true;
+    }
+
+    virtual void onDisconnect(IPAddress addr, String client_id) {
+        Serial.println(addr.toString() + " (" + client_id + ") disconnected");
+    }
+
+    virtual bool onAuth(String username, String password, String client_id) {
+        Serial.println("Username/Password/ClientId: " + username + "/" + password + "/" + client_id);
+        return true;
+    }
+    char buffer[256];
+    virtual void onData(String topic, const char *data, uint32_t length) {
+        char data_str[length + 1];
+        os_memcpy(data_str, data, length);
+        data_str[length] = '\0';
+
+        if (topic == "weather-station/outdoor1/wifi/rssi") {
+            outdoorLastSeenMillis = millis();
+            outdoorWifiRssi = atoi(data_str);
+        } else if (topic == "weather-station/outdoor1/battery/voltage") {
+            outdoorBatteryVoltage = atof(data_str);
+        } else if (topic == "weather-station/outdoor1/battery/percent") {
+            outdoorBatteryPercent = atof(data_str);
+        } else if (topic == "weather-station/outdoor1/uptime/milliseconds") {
+            outdoorUptimeMillis = atoi(data_str);
+        } else if (topic == "weather-station/outdoor1/sensors/temperature/celsius") {
+            outdoorSensorTempCelsus = atof(data_str);
+        } else if (topic == "weather-station/outdoor1/sensors/humidity/percent") {
+            outdoorSensorHumidityPercent = atof(data_str);
+        } else if (topic == "weather-station/outdoor1/sensors/pressure/hPa") {
+            outdoorSensorPressurehPa = atof(data_str);
+        }
+        Serial.println("received topic '" + topic + "' with data '" + (String)data_str + "'");
+        //printClients();
+    }
+
+    // Sample for the usage of the client info methods
+
+    virtual void printClients() {
+        for (int i = 0; i < getClientCount(); i++) {
+            IPAddress addr;
+            String client_id;
+
+            getClientAddr(i, addr);
+            getClientId(i, client_id);
+            Serial.println("Client " + client_id + " on addr: " + addr.toString());
+        }
+    }
+};
+
+myMQTTBroker myBroker;
+
+Adafruit_Si7021 sensor = Adafruit_Si7021();
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET 0 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+OneButton buttonA = OneButton(
+    D3,   // Input pin for the button
+    true, // Button is active LOW
+    true  // Enable internal pull-up resistor
+);
+
+OneButton buttonB = OneButton(
+    D4,   // Input pin for the button
+    true, // Button is active LOW
+    true  // Enable internal pull-up resistor
+);
+
+void startWiFiClient() {
+    Serial.println("Connecting to " + (String)ssid);
+    WiFi.mode(WIFI_STA);
+    WiFi.hostname("Weather-Station-Indoor");
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.println("WiFi connected");
+    Serial.println("IP address: " + WiFi.localIP().toString());
+}
+
+void startWiFiAP() {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, pass);
+    Serial.println("AP started");
+    Serial.println("IP address: " + WiFi.softAPIP().toString());
+}
+
+void setup() {
+    Serial.begin(74880);
+    Serial.println();
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;)
+            ; // Don't proceed, loop forever
+    }
+
+    Serial.print("Found model ");
+    switch (sensor.getModel()) {
+    case SI_Engineering_Samples:
+        Serial.print("SI engineering samples");
+        break;
+    case SI_7013:
+        Serial.print("Si7013");
+        break;
+    case SI_7020:
+        Serial.print("Si7020");
+        break;
+    case SI_7021:
+        Serial.print("Si7021");
+        break;
+    case SI_UNKNOWN:
+    default:
+        Serial.print("Unknown");
+    }
+
+    Serial.print(" Rev(");
+    Serial.print(sensor.getRevision());
+    Serial.print(")");
+    Serial.print(" Serial #");
+    Serial.print(sensor.sernum_a, HEX);
+    Serial.println(sensor.sernum_b, HEX);
+
+    display.clearDisplay();
+
+    display.setTextSize(3);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+
+    display.println("Weather");
+    display.println("Station");
+    display.setTextSize(1);
+    display.println("Booting...");
+    display.display();
+
+    if (WiFiAP)
+        startWiFiAP();
+    else
+        startWiFiClient();
+
+    Serial.println("Starting MQTT broker");
+    myBroker.init();
+
+    myBroker.subscribe("#");
+}
+
+int heaterCounter = 0;
+bool enableHeater = false;
+
+void loop() {
+    buttonA.tick();
+    buttonB.tick();
+
+    myBroker.publish("broker/clients/count", (String)myBroker.getClientCount());
+    for (int i = 0; i < myBroker.getClientCount(); i++) {
+        IPAddress addr;
+        String client_id;
+
+        myBroker.getClientAddr(i, addr);
+        myBroker.getClientId(i, client_id);
+
+        myBroker.publish("broker/clients/" + (String)client_id, (String)addr.toString());
+    }
+
+    myBroker.publish("broker/uptime", (String)millis());
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+
+    if (buttonA.isLongPressed()) {
+        display.println("This station");
+        String upTimeString;
+        if (millis() > 86400000) {
+            upTimeString = String(millis() / 86400000) + "days";
+        } else {
+            upTimeString = String((millis() / 1000) / 60) + "min";
+        }
+        long lastSeenSeconds = (millis() - outdoorLastSeenMillis) / 1000;
+
+        display.println("Uptime: " + upTimeString);
+        int wifi = 100 - WiFi.RSSI() * -1;
+        display.println("WLN " + String(wifi) + "% Clients " + myBroker.getClientCount());
+        display.println();
+        display.println("Temperature " + String(sensor.readTemperature()) + "C");
+        display.println("Humidity " + String(sensor.readHumidity()) + "%");
+    } else {
+        display.println("Outdoor station 1");
+        String upTimeString;
+        if (outdoorUptimeMillis > 86400000) {
+            upTimeString = String(outdoorUptimeMillis / 86400000) + "days";
+        } else {
+            upTimeString = String((outdoorUptimeMillis / 1000) / 60) + "min";
+        }
+        long lastSeenSeconds = (millis() - outdoorLastSeenMillis) / 1000;
+
+        display.println(String(lastSeenSeconds) + "s ago, Up: " + upTimeString);
+        int wifi = 100 - outdoorWifiRssi * -1;
+        display.println("WLN " + String(wifi) + "% Bat " + String((int)outdoorBatteryPercent) + "% " + String(outdoorBatteryVoltage) + "V");
+        display.println();
+        display.println("Temperature " + String(outdoorSensorTempCelsus) + "C");
+        display.println("Humidity " + String(outdoorSensorHumidityPercent) + "%");
+        display.println("Barometer " + String(outdoorSensorPressurehPa) + "hPa");
+    }
+
+    if (++heaterCounter == 30) {
+        enableHeater = !enableHeater;
+        sensor.heater(enableHeater);
+        Serial.print("Heater Enabled State: ");
+        if (sensor.isHeaterEnabled())
+            Serial.println("ENABLED");
+        else
+            Serial.println("DISABLED");
+
+        heaterCounter = 0;
+    }
+
+    display.display();
+
+    delay(1000);
+}
---
+++ arduino-weather-outdoor/src/main.cpp
@@ -0,0 +1,214 @@
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include <Wire.h>
+
+//for LED status
+#include <Ticker.h>
+Ticker ticker;
+
+const char *SSID = "";
+const char *PSK = "";
+const char *MQTT_BROKER = "Weather-Station-Indoor.greenkingdom";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme;
+
+// CRC function used to ensure data validity
+uint32_t calculateCRC32(const uint8_t *data, size_t length);
+
+// helper function to dump memory contents as hex
+void printMemory();
+
+// Structure which will be stored in RTC memory.
+// First field is CRC32, which is calculated based on the
+// rest of structure contents.
+// Any fields can go after CRC32.
+// We use byte array as an example.
+struct {
+    unsigned long millis;
+} rtcDataStruct;
+
+struct {
+    uint32_t crc32;
+    byte data[508];
+} rtcData;
+
+void tick() {
+    //toggle state
+    int state = digitalRead(BUILTIN_LED); // get the current state of GPIO1 pin
+    digitalWrite(BUILTIN_LED, !state);    // set pin to the opposite state
+}
+
+void setup() {
+    Serial.begin(74880);
+
+    pinMode(BUILTIN_LED, OUTPUT);
+    ticker.attach(0.6, tick);
+
+    // Read struct from RTC memory
+    if (ESP.rtcUserMemoryRead(0, (uint32_t *)&rtcData, sizeof(rtcData))) {
+         Serial.println("Reading rtcData");
+        //Serial.println("Read: ");
+        //printMemory();
+        //Serial.println();
+        uint32_t crcOfData = calculateCRC32((uint8_t *)&rtcData.data[0], sizeof(rtcData.data));
+        Serial.print("CRC32 of data: ");
+        Serial.println(crcOfData, HEX);
+        Serial.print("CRC32 read from RTC: ");
+        Serial.println(rtcData.crc32, HEX);
+        if (crcOfData != rtcData.crc32) {
+            Serial.println("CRC32 in RTC memory doesn't match CRC32 of data. Data is probably invalid!");
+        } else {
+            Serial.println("CRC32 check ok, data is probably valid.");
+            memcpy(&rtcDataStruct, rtcData.data, sizeof(rtcDataStruct));
+        }
+    }
+
+    Serial.println("Connecting to WiFi" + String(SSID));
+
+    WiFi.hostname("Weather-Station-Outdoor");
+    WiFi.begin(SSID, PSK);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    Serial.println("Connected, my IP is:");
+    Serial.println(WiFi.localIP());
+
+    client.setServer(MQTT_BROKER, 1883);
+
+    if (!bme.begin(0x76)) {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    }
+
+    bme.setSampling(Adafruit_BME280::MODE_FORCED,
+                    Adafruit_BME280::SAMPLING_X1, // temperature
+                    Adafruit_BME280::SAMPLING_X1, // pressure
+                    Adafruit_BME280::SAMPLING_X1, // humidity
+                    Adafruit_BME280::FILTER_OFF);
+
+    pinMode(A0, INPUT);
+}
+
+unsigned int lowest = 9000;
+char itoaBuf[64];
+char dtostrfBuf[64];
+
+const int deepSleepMillis = 5000;
+
+void updateSystemStats() {
+    long rssi = WiFi.RSSI();
+    client.publish("weather-station/outdoor1/wifi/rssi", itoa(rssi, itoaBuf, 10));
+    Serial.println("RSSI: " + String(rssi));
+
+    unsigned int raw = analogRead(A0);
+    client.publish("weather-station/outdoor1/battery/raw", itoa(raw, itoaBuf, 10));
+
+    if (raw < lowest)         lowest = raw;
+    float batPercent = lowest / 1024.0;
+    client.publish("weather-station/outdoor1/battery/percent", dtostrf(batPercent * 100, 4, 3, dtostrfBuf));
+
+    float batVoltage = batPercent * 4.1;
+    client.publish("weather-station/outdoor1/battery/voltage", dtostrf(batVoltage, 4, 3, dtostrfBuf));
+    Serial.println("Battery: " + String(batVoltage) + "V (raw: " + String(raw) + ")");
+    
+    client.publish("weather-station/outdoor1/uptime/milliseconds", itoa(rtcDataStruct.millis + deepSleepMillis + millis(), itoaBuf, 10));
+}
+
+void updateSensor() {
+    float temperature = bme.readTemperature();
+    client.publish("weather-station/outdoor1/sensors/temperature/celsius", dtostrf(temperature, 4, 2, dtostrfBuf));
+    Serial.println("Temperature = " + String(temperature) + "*C");
+
+    float humidity = bme.readHumidity();
+    client.publish("weather-station/outdoor1/sensors/humidity/percent", dtostrf(humidity, 4, 2, dtostrfBuf));
+    Serial.println("Humidity = " + String(humidity) + "%");
+
+    float pressure = bme.readPressure() / 100.0F;
+    client.publish("weather-station/outdoor1/sensors/pressure/hPa", dtostrf(pressure, 4, 2, dtostrfBuf));
+    Serial.println("Pressure = " + String(pressure) + "hpa");
+
+    //float altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+    //client.publish("weather-station/outdoor1/sensors/pressure/altitude", dtostrf(altitude, 4, 2, dtostrfBuf));
+    //Serial.println("Approx. Altitude = " + String(altitude) + "m");
+}
+
+void loop() {
+    if (!client.connected()) {
+        ticker.attach(0.6, tick);
+        while (!client.connected()) {
+            client.connect("Weather-Station-Outdoor");
+            delay(500);
+        }
+        ticker.detach();
+        digitalWrite(BUILTIN_LED, LOW);
+    }
+
+    updateSystemStats();
+
+    updateSensor();
+
+    client.loop();
+
+    delay(1000);
+
+    // Generate new data set for the struct
+    rtcDataStruct.millis = rtcDataStruct.millis + millis();
+    memcpy(rtcData.data, &rtcDataStruct, sizeof(rtcDataStruct));
+
+    // Update CRC32 of data
+    rtcData.crc32 = calculateCRC32((uint8_t *)&rtcData.data[0], sizeof(rtcData.data));
+    // Write struct to RTC memory
+    if (ESP.rtcUserMemoryWrite(0, (uint32_t *)&rtcData, sizeof(rtcData))) {
+        Serial.println("rtcData written");
+        //Serial.println("Write: ");
+        //printMemory();
+        //Serial.println();
+    }
+
+    unsigned long int actSleepTime = deepSleepMillis * 1000; //microseconds
+
+    Serial.println("Going to sleep for " + String(deepSleepMillis) + "ms");
+    ESP.deepSleep(actSleepTime);
+}
+
+uint32_t calculateCRC32(const uint8_t *data, size_t length) {
+    uint32_t crc = 0xffffffff;
+    while (length--) {
+        uint8_t c = *data++;
+        for (uint32_t i = 0x80; i > 0; i >>= 1) {
+            bool bit = crc & 0x80000000;
+            if (c & i) {
+                bit = !bit;
+            }
+            crc <<= 1;
+            if (bit) {
+                crc ^= 0x04c11db7;
+            }
+        }
+    }
+    return crc;
+}
+
+//prints all rtcData, including the leading crc32
+void printMemory() {
+    char buf[3];
+    uint8_t *ptr = (uint8_t *)&rtcData;
+    for (size_t i = 0; i < sizeof(rtcData); i++) {
+        sprintf(buf, "%02X", ptr[i]);
+        Serial.print(buf);
+        if ((i + 1) % 32 == 0) {
+            Serial.println();
+        } else {
+            Serial.print(" ");
+        }
+    }
+    Serial.println();
+}
